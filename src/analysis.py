941aa8747bf72713e07f2c0a8e350dc462b218ed
VladVynnytskyi <vynnytskyivladyslav@gmail.com>
2025-06-04 23:05:37 +0200

    Add max/min and avr temperature Lviv

--- src/analysis.py
+++ src/analysis.py
@@ -1,3 +1,23 @@
+import pandas as pd
+
+
+def get_yearly_avg(df):
+    """Return average temperature grouped by year"""
+    return df.groupby(df['date'].dt.year)['temperature'].mean()
+
+def get_max_anomaly_city(df):
+    """Return max temperature and its date"""
+    max_temp = df['temperature'].max()
+    date = df[df['temperature'] == max_temp]['date'].values[0]
+    return max_temp, pd.to_datetime(date).date()
+
+
+def get_min_anomaly_city(df):
+    """Return min temperature and its date"""
+    min_temp = df['temperature'].min()
+    date = df[df['temperature'] == min_temp]['date'].values[0]
+    return min_temp, pd.to_datetime(date).date()
+
 def show_yearly_stats(df):
     #Display yearly average temperatures, and the dates of max/min temperatures.
 
@@ -12,3 +32,17 @@ def show_yearly_stats(df):
     min_temp = df['temperature'].min()
     min_date = df[df['temperature'] == min_temp]['date'].iloc[0]
     print(f" Lowest temperature: {min_temp}°C on {min_date.date()}")
+
+def show_city_stats(df, city_name="City"):
+    
+    #Show yearly average temperature and extreme anomalies for a given city.
+
+    yearly = get_yearly_avg(df)
+    print(f"\n Average yearly temperature in {city_name}:")
+    print(yearly.round(2))
+
+    max_temp, max_date = get_max_anomaly_city(df)
+    min_temp, min_date = get_min_anomaly_city(df)
+
+    print(f"\n {city_name} - Highest temperature: {max_temp}°C on {max_date}")
+    print(f" {city_name} - Lowest temperature: {min_temp}°C on {min_date}")
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from data_loader import load_and_clear, load_city_data
 from visualizer import plot_monthly_box, plot_yearly_avg, plot_rolling_avg, plot_anomalies, plot_compare_cities
-from analysis import show_yearly_stats
+from analysis import show_yearly_stats, show_city_stats
 
 # Load Krakow weather data from CSV file
 
@@ -27,4 +27,6 @@ df_krakow = load_and_clear("data/krakow_weather.csv")
 df_lviv = load_city_data("data/lviv_weather.csv")
 
 plot_compare_cities(df_krakow, df_lviv)
+
+show_city_stats(df_lviv, city_name="Lviv")
 
